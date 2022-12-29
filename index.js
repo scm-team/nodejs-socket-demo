4954e448bf4b65db3a87bac6dbce26b5b486ed0b
WaiYanLin71 <scm.waiyanlinn@gmail.com>
2022-12-29 08:42:38 +0630

    format date and added duration

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ const app = express();
 const cors = require('cors');
 const http = require('http');
 const dotenv = require('dotenv');
+const dayjs = require('dayjs');
+const localizedFormat = require('dayjs/plugin/localizedFormat')
+dayjs.extend(localizedFormat)
 dotenv.config();
 const server = http.createServer(app);
 const { Server } = require("socket.io");
@@ -35,13 +38,13 @@ const connection = mysql.createConnection({
 });
 
 io.on('connection', (socket) => {
-    
+
     console.log(`Socket connect with id: ${socket.id}`);
-    
+
     socket.on("disconnect", (reason) => {
         console.log(`Socket disconnect ${socket.id} due to ${reason}`);
     });
-    
+
     connection.query(
         'SELECT * FROM `votes`',
         function (err, results) {
@@ -54,8 +57,8 @@ io.on('connection', (socket) => {
             }
         }
     );
-    
-    socket.on('stop',function(){
+
+    socket.on('stop', function () {
         connection.query('UPDATE  votes SET end_time=now()', function (error, results, fields) {
             if (error) {
                 console.log(error);
@@ -66,10 +69,36 @@ io.on('connection', (socket) => {
                 'SELECT * FROM `votes`',
                 function (err, results) {
                     if (!err) {
+                        const startTime = new Date(results[0]?.start_time);
+                        const endTime = new Date(results[0]?.end_time);
+
+                        var delta = Math.abs(startTime - endTime) / 1000;
+
+                        // calculate (and subtract) whole days
+                        var days = Math.floor(delta / 86400);
+                        delta -= days * 86400;
+
+                        // calculate (and subtract) whole hours
+                        var hours = Math.floor(delta / 3600) % 24;
+                        delta -= hours * 3600;
+
+                        // calculate (and subtract) whole minutes
+                        var minutes = Math.floor(delta / 60) % 60;
+                        delta -= minutes * 60;
+
+                        // what's left is seconds
+                        var seconds = delta % 60;
+
                         socket.broadcast.emit('stopVote', {
                             count: results[0]?.count,
-                            start_time: results[0]?.start_time,
-                            end_time: results[0]?.end_time,
+                            start_time: dayjs(startTime).format('llll'),
+                            end_time:  dayjs(endTime).format('llll'),
+                            diff:{
+                                days,
+                                hours,
+                                minutes,
+                                seconds
+                            }
                         })
                     }
                 }
@@ -88,7 +117,7 @@ io.on('connection', (socket) => {
                 'SELECT * FROM `votes`',
                 function (err, results) {
                     if (!err) {
-                        io.emit('resetVoteToClient', {
+                        socket.broadcast.emit('resetVoteToClient', {
                             count: results[0]?.count,
                             start_time: results[0]?.start_time,
                             end_time: results[0]?.end_time,
@@ -99,19 +128,44 @@ io.on('connection', (socket) => {
         });
     })
 
-    socket.on('vote', function () {
-        connection.query("UPDATE  votes SET  count = count + 1 , start_time=now(),end_time=null WHERE id=1", function (error, results, fields) {
-            if (error) {
-                console.log(error)
-            }
-        });
+    socket.on('vote', function (isStarted) {
+        if (isStarted) {
+            connection.query(
+                'SELECT * FROM `votes`',
+                function (err, results) {
+                    if (results) {
+                        if (!results[0]?.start_time) {
+                            connection.query("UPDATE  votes SET  count = count + 1 , start_time=now(),end_time=null WHERE id=1", function (error, results, fields) {
+                                if (error) {
+                                    console.log(error)
+                                }
+                            });
+                        } else {
+                            connection.query("UPDATE  votes SET  count = count + 1 ,end_time=null WHERE id=1", function (error, results, fields) {
+                                if (error) {
+                                    console.log(error)
+                                }
+                            });
+                        }
+                    }
+
+                }
+            );
+        } else {
+            connection.query("UPDATE  votes SET  count = count + 1 , start_time=null,end_time=null WHERE id=1", function (error, results, fields) {
+                if (error) {
+                    console.log(error)
+                }
+            });
+        }
+
         connection.query(
             'SELECT * FROM `votes`',
             function (err, results) {
                 if (results) {
                     socket.broadcast.emit('voteToClient', {
                         count: results[0]?.count,
-                        start_time: results[0]?.start_time,
+                        start_time: results[0]?.start_time ? dayjs(results[0]?.start_time).format('llll') : '',
                         end_time: results[0]?.end_time,
                     })
                 }
